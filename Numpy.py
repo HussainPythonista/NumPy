bc9b44326c6324f786c6ec7b836d8bb750977a73
Mohamed Kalanthar Hussain <60617460+HussainPythonista@users.noreply.github.com>
2021-03-04 20:41:58 +0530

    FIrst File Created using Colaboratory

---
+++ Numpy.py
@@ -0,0 +1,53 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Untitled1.ipynb",
+      "provenance": [],
+      "authorship_tag": "ABX9TyMUYjIujG/MrCPHXdoOC5fo",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/HussainPythonista/DATA-STRUCTURE-AND-ALGORITHMS/blob/master/Numpy.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "tkk0LaZYxFy0",
+        "outputId": "e87fcc81-24a7-40f7-a704-283fd87ac4cf"
+      },
+      "source": [
+        "import numpy as np\n",
+        "arangeValue=np.arange(1,10,2,dtype=\"float\")\n",
+        "print(arangeValue)"
+      ],
+      "execution_count": 5,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "[1. 3. 5. 7. 9.]\n"
+          ],
+          "name": "stdout"
+        }
+      ]
+    }
+  ]
+}
